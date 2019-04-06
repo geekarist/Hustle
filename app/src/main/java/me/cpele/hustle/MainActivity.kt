4444c26e4eb6daacd591ae619beeeb6d8ef21362
Christobal <geekarist@users.noreply.github.com>
2019-04-06 11:10:30 +0200

    Create MainViewModel

--- app/src/main/java/me/cpele/hustle/MainActivity.kt
+++ app/src/main/java/me/cpele/hustle/MainActivity.kt
@@ -1,12 +1,35 @@
 package me.cpele.hustle
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel by lazy {
+        ViewModelProviders.of(
+            this,
+            ViewModelProvider.AndroidViewModelFactory(application)
+        ).get(MainViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel.strTimeData.observe(this, Observer {
+            main_display_text.text = it
+        })
+
+        viewModel.playPauseLabelData.observe(this, Observer {
+            main_play_pause_button.text = it
+        })
+
+        main_play_pause_button.setOnClickListener {
+            viewModel.onTogglePlayPause()
+        }
     }
 }
---
+++ app/src/main/java/me/cpele/hustle/MainViewModel.kt
@@ -0,0 +1,25 @@
+package me.cpele.hustle
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val _strTimeData =
+        MutableLiveData<String>().apply { value = "00:00" }
+    val strTimeData: LiveData<String> get() = _strTimeData
+
+    private val isPlayingData = MutableLiveData<Boolean>().apply { value = false }
+
+    val playPauseLabelData: LiveData<String> = Transformations.map(isPlayingData) {
+        if (it) application.getString(R.string.main_pause)
+        else application.getString(R.string.main_play)
+    }
+
+    fun onTogglePlayPause() {
+        isPlayingData.value = isPlayingData.value != true
+    }
+}
