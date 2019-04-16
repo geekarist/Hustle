70546e82ac8a58f9e606df533a90ac2bc2da7fd9
Christobal <geekarist@users.noreply.github.com>
2019-04-16 08:25:32 +0200

    Display Up menu item

--- app/src/main/java/me/cpele/hustle/app/DebugSettingsActivity.kt
+++ app/src/main/java/me/cpele/hustle/app/DebugSettingsActivity.kt
@@ -1,12 +1,13 @@
 package me.cpele.hustle.app
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import me.cpele.hustle.R
 
-class DebugSettingsActivity : Activity() {
+class DebugSettingsActivity : AppCompatActivity() {
 
     companion object {
         fun start(context: Context) {
@@ -18,5 +19,15 @@ class DebugSettingsActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_debug_settings)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean =
+        when (item?.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
 }
