e8d09bf31c0355f47ac23c102b60f11a3bb18f0a
Christobal <geekarist@users.noreply.github.com>
2019-04-22 15:15:30 +0200

    Implement FirebaseLoginActivity

--- app/src/main/java/me/cpele/hustle/app/FirebaseLoginActivity.kt
+++ app/src/main/java/me/cpele/hustle/app/FirebaseLoginActivity.kt
@@ -1,17 +1,21 @@
 package me.cpele.hustle.app
 
+import android.app.Activity
 import android.app.Application
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toast
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import java.util.concurrent.Executors
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import me.cpele.hustle.R
 
 class FirebaseLoginActivity : AppCompatActivity() {
 
     companion object {
         const val ACTION_FIREBASE_LOGIN_SUCCESS = "me.cpele.hustle.ACTION_FIREBASE_LOGIN_SUCCESS"
+        private const val REQUEST_SIGN_IN = 42
 
         fun start(application: Application) =
             application.startActivity(Intent(application, FirebaseLoginActivity::class.java))
@@ -19,26 +23,31 @@ class FirebaseLoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(TextView(this).apply { text = "Fake Firebase login" })
-        val executor = Executors.newSingleThreadExecutor()
-        executor.submit {
-            runOnUiThread {
-                Toast.makeText(
-                    application,
-                    "Please wait while logging in...",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-            Thread.sleep(20000)
-            runOnUiThread {
-                Toast.makeText(
-                    application,
-                    "OK, logged in...",
-                    Toast.LENGTH_SHORT
-                ).show()
+        setContentView(R.layout.activity_firebase_login)
+
+        val intent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(
+                listOf(
+                    AuthUI.IdpConfig.GoogleBuilder().build()
+                )
+            ).build()
+
+        startActivityForResult(intent, REQUEST_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+
+                Log.d(javaClass.simpleName, "Logged in as $user")
                 sendBroadcast(Intent(ACTION_FIREBASE_LOGIN_SUCCESS))
                 finish()
             }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
         }
     }
 }
