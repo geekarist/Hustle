585374fe295a778dbf11431e0e7a97882f719128
Christobal <geekarist@users.noreply.github.com>
2019-04-26 15:25:49 +0200

    Implement BeeminderLoginActivity without AppAuth

--- app/src/main/java/me/cpele/hustle/app/BeeminderLoginActivity.kt
+++ app/src/main/java/me/cpele/hustle/app/BeeminderLoginActivity.kt
@@ -2,10 +2,13 @@ package me.cpele.hustle.app
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabsIntent
+import me.cpele.hustle.BuildConfig
 import me.cpele.hustle.R
-import java.util.concurrent.Executors
 
 class BeeminderLoginActivity : AppCompatActivity() {
 
@@ -13,14 +16,25 @@ class BeeminderLoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_beeminder_login)
 
-        val executor = Executors.newSingleThreadExecutor()
-        executor.submit {
-            Thread.sleep(5000)
-            runOnUiThread {
-                sendBroadcast(Intent(ACTION_BEEMINDER_LOGIN_SUCCESS))
-                finish()
-            }
+        if (intent?.data?.scheme == "hustle" && intent?.data?.host == "redirect") {
+            Toast.makeText(this, "Logged in!", Toast.LENGTH_SHORT).show()
+        } else {
+            val uri = Uri.parse(BuildConfig.BEEMINDER_AUTH_ENDPOINT)
+                .buildUpon()
+                .appendQueryParameter("client_id", BuildConfig.BEEMINDER_CLIENT_ID)
+                .appendQueryParameter("redirect_uri", BuildConfig.BEEMINDER_REDIRECT_URI)
+                .appendQueryParameter("response_type", "token")
+                .build()
+
+            CustomTabsIntent.Builder()
+                .build()
+                .launchUrl(this, uri)
         }
+    }
+
+    private fun complete() {
+        sendBroadcast(Intent(ACTION_BEEMINDER_LOGIN_SUCCESS))
+        finish()
     }
 
     companion object {
