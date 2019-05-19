e412af9c4ca17e602acc8f2c78380969b96d75e1
Christobal <geekarist@users.noreply.github.com>
2019-05-19 14:45:53 +0200

    Use Application not CustomApplication

--- app/src/main/java/me/cpele/hustle/app/AndroidStringProvider.kt
+++ app/src/main/java/me/cpele/hustle/app/AndroidStringProvider.kt
@@ -1,10 +1,11 @@
 package me.cpele.hustle.app
 
+import android.app.Application
 import me.cpele.hustle.R
 import me.cpele.hustle.domain.EggTimer
 
 class AndroidStringProvider(
-    private val customApplication: CustomApplication
+    private val customApplication: Application
 ) : EggTimer.StringProvider {
 
     override val mainPause: String get() = customApplication.getString(R.string.main_pause)
